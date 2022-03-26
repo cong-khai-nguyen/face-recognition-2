3999dccddb9e80b002074f2c25106d637e4b5aa1
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-26 13:00:03 -0500

    function to encode faces we train on

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import cv2
 import numpy as np
+import os
 import face_recognition as fr
 
+# function to encodes all the faces in the faces folder
+# return a dict of (name, image encoded) -> Ex: bill gate : [......]
+def get_encoded_faces():
+    encoded = {}
+
+    for dirpath, dnames, file_names in os.walk("./media/faces"):
+
+        for f in file_names:
+            if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg"):
+                face = fr.load_image_file("media/faces/" + f)
+                encoding = fr.face_encodings(face)[0]
+                encoded[f.split(".")[0]] = encoding
+
+    return encoded
